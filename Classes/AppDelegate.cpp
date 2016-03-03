07a7ce93ffef736179cddc62e3f9ae711c9cadd9
Susan <416565205@qq.com>
2016-03-03 17:53:44 +0800

    Add SomeThing

---
+++ Classes/AppDelegate.cpp
@@ -0,0 +1,234 @@
+#include "AppDelegate.h"
+
+#include "SimpleAudioEngine.h"
+#include "jsb_cocos2dx_auto.hpp"
+#include "jsb_cocos2dx_ui_auto.hpp"
+#include "jsb_cocos2dx_studio_auto.hpp"
+#include "jsb_cocos2dx_builder_auto.hpp"
+#include "jsb_cocos2dx_spine_auto.hpp"
+#include "jsb_cocos2dx_extension_auto.hpp"
+#include "jsb_cocos2dx_3d_auto.hpp"
+#include "jsb_cocos2dx_3d_extension_auto.hpp"
+#include "3d/jsb_cocos2dx_3d_manual.h"
+#include "ui/jsb_cocos2dx_ui_manual.h"
+#include "cocostudio/jsb_cocos2dx_studio_manual.h"
+#include "cocosbuilder/js_bindings_ccbreader.h"
+#include "spine/jsb_cocos2dx_spine_manual.h"
+#include "extension/jsb_cocos2dx_extension_manual.h"
+#include "localstorage/js_bindings_system_registration.h"
+#include "chipmunk/js_bindings_chipmunk_registration.h"
+#include "jsb_opengl_registration.h"
+#include "network/XMLHTTPRequest.h"
+#include "network/jsb_websocket.h"
+#include "network/jsb_socketio.h"
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
+#include "platform/android/CCJavascriptJavaBridge.h"
+#elif (CC_TARGET_PLATFORM == CC_PLATFORM_IOS || CC_TARGET_PLATFORM == CC_PLATFORM_MAC)
+#include "platform/ios/JavaScriptObjCBridge.h"
+#endif
+
+#include "CodeIDESupport.h"
+#include "Runtime.h"
+#include "ConfigParser.h"
+
+#include "MobClickCpp.h"
+//热更新相关
+#include "platform/CCFileUtils.h"
+#include <iostream>
+#include <fstream>
+#include <ctime>
+
+USING_NS_CC;
+
+#define UAPPKEY "5593a55967e58ed391000c95"
+using namespace CocosDenshion;
+
+AppDelegate::AppDelegate()
+{
+}
+
+AppDelegate::~AppDelegate()
+{
+    ScriptEngineManager::destroyInstance();
+#if (COCOS2D_DEBUG > 0 && CC_CODE_IDE_DEBUG_SUPPORT > 0)
+	// NOTE:Please don't remove this call if you want to debug with Cocos Code IDE
+	endRuntime();
+#endif
+
+	ConfigParser::purge();
+}
+//热更新相关，根据不同的渠道选择不同的参数打包
+void AppDelegate::copyFile(){
+    
+    std::string filePath = FileUtils::getInstance()->getWritablePath()+"project.manifest";
+    if(!FileUtils::getInstance()->isFileExist(filePath)){
+        std::string fileValue;
+        fileValue = FileUtils::getInstance()->getStringFromFile("res/project.manifest");
+//        CCLOG("%ld====%ld===%ld",sizeof(char),sizeof(fileValue.size()),fileValue.size());
+        FILE * pFile;
+        pFile = fopen (filePath.c_str(), "wb");
+        fwrite (fileValue.c_str() , sizeof(char), fileValue.size(), pFile);
+        fclose (pFile);
+    }
+}
+void AppDelegate::initGLContextAttrs()
+{
+    GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8};
+    
+    GLView::setGLContextAttrs(glContextAttrs);
+}
+
+bool AppDelegate::applicationDidFinishLaunching()
+{
+    copyFile();
+#if (COCOS2D_DEBUG > 0 && CC_CODE_IDE_DEBUG_SUPPORT > 0)
+    // NOTE:Please don't remove this call if you want to debug with Cocos Code IDE
+    initRuntime();
+#endif
+    // initialize director
+    auto director = Director::getInstance();
+    auto glview = director->getOpenGLView();    
+    if(!glview) {
+        Size viewSize = ConfigParser::getInstance()->getInitViewSize();
+        string title = ConfigParser::getInstance()->getInitViewName();
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32 || CC_TARGET_PLATFORM == CC_PLATFORM_MAC) && (COCOS2D_DEBUG > 0 && CC_CODE_IDE_DEBUG_SUPPORT > 0)
+        extern void createSimulator(const char* viewName, float width, float height,bool isLandscape = true, float frameZoomFactor = 1.0f);
+        bool isLanscape = ConfigParser::getInstance()->isLanscape();
+        createSimulator(title.c_str(), viewSize.width,viewSize.height, isLanscape);
+#else
+        glview = cocos2d::GLViewImpl::createWithRect(title.c_str(), Rect(0, 0, viewSize.width, viewSize.height));
+        director->setOpenGLView(glview);
+#endif
+    }
+
+    // set FPS. the default value is 1.0/60 if you don't call this
+    director->setAnimationInterval(1.0 / 60);
+    
+    ScriptingCore* sc = ScriptingCore::getInstance();
+    sc->addRegisterCallback(register_all_cocos2dx);
+    sc->addRegisterCallback(register_cocos2dx_js_core);
+    sc->addRegisterCallback(jsb_register_system);
+
+    // extension can be commented out to reduce the package
+    sc->addRegisterCallback(register_all_cocos2dx_extension);
+    sc->addRegisterCallback(register_all_cocos2dx_extension_manual);
+
+    // chipmunk can be commented out to reduce the package
+    sc->addRegisterCallback(jsb_register_chipmunk);
+    // opengl can be commented out to reduce the package
+    sc->addRegisterCallback(JSB_register_opengl);
+    
+    // builder can be commented out to reduce the package
+    sc->addRegisterCallback(register_all_cocos2dx_builder);
+    sc->addRegisterCallback(register_CCBuilderReader);
+    
+    // ui can be commented out to reduce the package, attension studio need ui module
+    sc->addRegisterCallback(register_all_cocos2dx_ui);
+    sc->addRegisterCallback(register_all_cocos2dx_ui_manual);
+
+    // studio can be commented out to reduce the package, 
+    sc->addRegisterCallback(register_all_cocos2dx_studio);
+    sc->addRegisterCallback(register_all_cocos2dx_studio_manual);
+    
+    // spine can be commented out to reduce the package
+    sc->addRegisterCallback(register_all_cocos2dx_spine);
+    sc->addRegisterCallback(register_all_cocos2dx_spine_manual);
+    
+    // XmlHttpRequest can be commented out to reduce the package
+    sc->addRegisterCallback(MinXmlHttpRequest::_js_register);
+    // websocket can be commented out to reduce the package
+    sc->addRegisterCallback(register_jsb_websocket);
+    // sokcet io can be commented out to reduce the package
+    sc->addRegisterCallback(register_jsb_socketio);
+    
+    // 3d can be commented out to reduce the package
+    sc->addRegisterCallback(register_all_cocos2dx_3d);
+    sc->addRegisterCallback(register_all_cocos2dx_3d_manual);
+    
+    // 3d extension can be commented out to reduce the package
+    sc->addRegisterCallback(register_all_cocos2dx_3d_extension);
+    
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
+    sc->addRegisterCallback(JavascriptJavaBridge::_js_register);
+#elif (CC_TARGET_PLATFORM == CC_PLATFORM_IOS|| CC_TARGET_PLATFORM == CC_PLATFORM_MAC)
+    sc->addRegisterCallback(JavaScriptObjCBridge::_js_register);
+#endif
+    
+    //读取客户端的配置信息
+    std::string version =  readConfigurationFile();
+    
+    
+    //U盟渠道统计
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
+    MOBCLICKCPP_START_WITH_APPKEY_AND_CHANNEL("5593a53667e58ea18f0014d9", version.c_str());
+
+#elif (CC_TARGET_PLATFORM == CC_PLATFORM_IOS|| CC_TARGET_PLATFORM == CC_PLATFORM_MAC)
+    
+    MOBCLICKCPP_START_WITH_APPKEY_AND_CHANNEL("5593a55967e58ed391000c95", version.c_str());
+
+#endif
+    
+    
+#if (COCOS2D_DEBUG > 0 && CC_CODE_IDE_DEBUG_SUPPORT > 0)
+    // NOTE:Please don't remove this call if you want to debug with Cocos Code IDE
+    startRuntime();
+#else
+    sc->start();
+    sc->runScript("script/jsb_boot.js");
+    auto engine = ScriptingCore::getInstance();
+    ScriptEngineManager::getInstance()->setScriptEngine(engine);
+    ScriptingCore::getInstance()->runScript(ConfigParser::getInstance()->getEntryFile().c_str());
+#endif
+    return true;
+}
+//读取渠道号
+std::string AppDelegate::readConfigurationFile(){
+    std::string qudaohao = "unknowChannel";
+    string File_path = "res/NN_Config.json";
+    //获得文件在系统的绝对路径
+    string filepath = FileUtils::getInstance()->fullPathForFilename(File_path.c_str());
+    if (!FileUtils::getInstance()->isFileExist(filepath)) {
+        CCLOG("ERROR: Could not read res/NN_Config.json");
+        return qudaohao;
+    };
+    //读取的内容
+    string data =FileUtils::getInstance()->getStringFromFile(filepath);
+    //读取动作XML文件
+    rapidjson::Document jsonDict;
+    jsonDict.Parse<0>(data.c_str());
+    if (jsonDict.HasParseError())
+    {
+        CCLOG("GetParseError %s\n", jsonDict.GetParseError());
+        return qudaohao;
+    }
+    if(jsonDict.HasMember("channelName")){;
+        qudaohao =jsonDict["channelName"].GetString();
+        CCLOG("readConfigurstion-qudaohao%s",qudaohao.c_str());
+    };
+    return qudaohao;
+}
+
+// This function will be called when the app is inactive. When comes a phone call,it's be invoked too
+void AppDelegate::applicationDidEnterBackground()
+{
+    umeng::MobClickCpp::applicationDidEnterBackground();
+    auto director = Director::getInstance();
+    director->stopAnimation();
+    director->pause();
+    director->getEventDispatcher()->dispatchCustomEvent("game_on_hide");
+    SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
+    SimpleAudioEngine::getInstance()->pauseAllEffects();
+}
+
+// this function will be called when the app is active again
+void AppDelegate::applicationWillEnterForeground()
+{
+    umeng::MobClickCpp::applicationWillEnterForeground();
+    auto director = Director::getInstance();
+    director->startAnimation();
+    director->resume();
+    director->getEventDispatcher()->dispatchCustomEvent("game_on_show");
+    SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
+    SimpleAudioEngine::getInstance()->resumeAllEffects();
+}
